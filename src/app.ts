0a6fb7562e04ac66658be4c92403ccdbee6f149b
Hmin25 <hmin225@gmail.com>
2026-05-02 15:18:07 +0800

    chore: allowed origin - cater windows

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,17 @@ import router from './routes';
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (ctx.method === 'OPTIONS') {
+    ctx.status = 204;
+    return;
+  }
+  await next();
+});
+
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
